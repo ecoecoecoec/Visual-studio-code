70b69df46127c47be7c5ad17d974257cc0dc28b2
ecoecoecoec <108348116+ecoecoecoec@users.noreply.github.com>
2022-07-02 16:16:02 +0400

    Add files via upload

---
+++ HTML programs/first.js
@@ -0,0 +1,18 @@
+
+function msg()
+{
+    document.write("you got a virus")
+        function show() {
+ 
+            /* Access image by id and change
+            the display property to block*/
+            document.getElementById('image')
+                    .style.display = "block";
+ 
+            document.getElementById('btnID')
+                    .style.display = "none";
+        }
+    </script>
+
+    }
+
